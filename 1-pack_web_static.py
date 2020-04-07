0bdc7df9c82f2f0eae6ec16265b2e9cb5f36d17a
RaudoR <rivaspaulino@outlook.com>
2020-04-07 01:00:42 +0000

    deploy web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,9 +4,9 @@
 from fabric.api import *
 from time import strftime as ti
 
-
 def do_pack():
-    """Fabric script to compress files in web_static"""
+    '''Fabric script to compress files in web_static'''
+
     local("mkdir -p versions")
     ver = ti("%Y%m%d%H%M%S")
     arc = local("tar -cvzf versions/web_static_{}.tgz web_static".format(ver))
