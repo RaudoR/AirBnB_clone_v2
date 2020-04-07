ef8c3758e38e8e0cda4d80d465876c4deeea64b3
RaudoR <rivaspaulino@outlook.com>
2020-04-07 00:25:43 +0000

    web static deployment

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# Web static deployment
+sudo apt-get install update
+sudo apt-get install -y nginx
+
+sudo mkdir -p /data/web_static/releases/test
+sudo mkdir -p /data/web_static/shared
+
+echo "Simple content" | sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i '39 i\   \tlocation /hbnb_static {\n\talias /data/web_static/current;\n}' /etc/nginx/sites-available/default
+
+sudo service nginx restart
