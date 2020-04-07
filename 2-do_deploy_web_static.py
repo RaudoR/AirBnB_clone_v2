697266b0dbe16649ff6d2411e899f7efb6741551
RaudoR <rivaspaulino@outlook.com>
2020-04-07 18:44:18 +0000

    deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,19 +8,35 @@ env.user = 'ubuntu'
 env.hosts = ['34.74.25.201', '54.173.62.214']
 
 
+def do_pack():
+    '''Fabric script to compress files in web_static'''
+
+    local("mkdir -p versions")
+    ver = ti("%Y%m%d%H%M%S")
+    arc = local("tar -cvzf versions/web_static_{}.tgz web_static".format(ver))
+
+    if arc is None:
+        return None
+    else:
+        return ("versions/web_static_{}".format(ver))
+
+
 def do_deploy(archive_path):
     '''deploy static to servers'''
-    if os.path.exists(archive_path):
-        new_path = archive_path[9:]
-        de_path = '/data/web_static/releases/{}/'.format(new_path)[0:-5]
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}'.format(de_path))
-        run('tar -xzf /tmp/{} -C {}'.format(new_path, de_path))
-        run('rm /tmp/{}'.format(new_path))
-        run('mv  {}/web_static/* {}'.format(de_path, de_path))
-        run('rm -rf {}/web_static'.format(de_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(de_path))
-        print('New version deployed successfully!')
+    if (os.path.isfile(archive_path) is False):
+        return False
+    path = archive_path.split('/')[1]
+    target = "/data/web_static/releases/" + path
+    try:
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}/".format(target))
+        run("sudo tar -xzf /tmp/{} -C {}/".format(path, target))
+        run("sudo rm /tmp/{}".format(path))
+        run("sudo mv {}/web_static/* {}/".format(target, target))
+        run("sudo rm -rf {}/web_static".format(target))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(target))
+        print("New version deployed")
         return True
-    return False
+    except Exception:
+        return False
