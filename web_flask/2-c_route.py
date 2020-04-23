338824fb05af962bc52e32d42e04816d1be79311
RaudoR <rivaspaulino@outlook.com>
2020-04-23 00:21:23 +0000

    task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''start flask server'''
+from flask import Flask
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    '''display text'''
+    return ("Hello HBNB!")
+
+
+@app.route('/hbnb')
+def HBNB():
+    '''deisplay text'''
+    return ("HBNB")
+
+
+@app.route('/c/<text>')
+def c(text):
+    '''display C followed by text'''
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
