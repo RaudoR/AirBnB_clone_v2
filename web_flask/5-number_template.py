5ca56442a8ac940b8fac8f9b92ef54df7e9f7249
RaudoR <rivaspaulino@outlook.com>
2020-04-23 00:53:19 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -39,9 +39,9 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>')
-def number_template(n):
+def htmlTemplate(n):
     '''display a HTML page'''
-    return render_template('5-number.html', number=n)
+    return (render_template('5-number.html', number=n))
 
 
 if __name__ == "__main__":
