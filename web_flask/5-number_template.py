0f34c877713231e133aab56d34e55da6634f5f00
RaudoR <rivaspaulino@outlook.com>
2020-04-30 19:01:39 +0000

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,48 +1,48 @@
 #!/usr/bin/python3
-'''start flask server'''
-from flask import Flask
-
-
+"""Python script that starts a flask application on 0.0.0.0 port 5000"""
+from flask import Flask, render_template
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
-@app.route("/", strict_slashes=False)
+@app.route('/')
 def hello_world():
-    '''display text'''
-    return ("Hello HBNB!")
+    """Python function to print Hello HBNB"""
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb')
-def HBNB():
-    '''deisplay text'''
-    return ("HBNB")
+def hbnb():
+    """Python function to print HBNB"""
+    return "HBNB"
 
 
 @app.route('/c/<text>')
-def c(text):
-    '''display C followed by text'''
-    return "C {}".format(text.replace('_', ' '))
+def c_is_fun(text):
+    """Python function to print C with directory"""
+    return "C " + text.replace('_', ' ')
 
 
-@app.route('/python/')
+@app.route('/python')
 @app.route('/python/<text>')
-def python(text="is cool"):
-    '''display Python, followed by text'''
-    return "Python {}".format(text.replace('_', ' '))
+def python_magic(text="is cool"):
+    """Python function to print Python magic with directory"""
+    return "Python " + text.replace('_', ' ')
 
 
 @app.route('/number/<int:n>')
-def number(n):
-    '''display n only if its an integer'''
-    return "{:d} is a number".format(n)
+def number_route(n):
+    """Python function to a number"""
+    if isinstance(n, int):
+        return str(n) + ' is a number'
 
 
 @app.route('/number_template/<int:n>')
-def htmlTemplate(n):
-    '''display a HTML page'''
-    return (render_template('5-number.html', number=n))
+def number_template(n=None):
+    """Python function to a number"""
+    if isinstance(n, int):
+        return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
