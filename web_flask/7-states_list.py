3cc7b53cce9e647d6702c3f548074727e2b7c322
RaudoR <rivaspaulino@outlook.com>
2020-05-02 03:50:37 +0000

    task 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-'''Python script that starts a flask application on 0.0.0.0 port 5000'''
+"""starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
+from models import storage, classes
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_app(exception):
-    """Calls Storage close on appcontext"""
+def teardown(excep):
+    """After each request you must remove the current SQLAlchemy Session"""
     storage.close()
 
 
 @app.route('/states_list')
 def states_list():
-    states = []
-    for key, values in storage.all('State').items():
-        states.append(values)
+    """display Hello HBNB!"""
+    states = storage.all(classes["State"]).values()
     return render_template('7-states_list.html', states=states)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run()
