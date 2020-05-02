0eef36cb03d1630fcbac49b6a2e1a5d1c02d0fe1
RaudoR <rivaspaulino@outlook.com>
2020-05-02 03:55:29 +0000

    task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
 '''Python script that starts a flask application on 0.0.0.0 port 5000'''
-from flask import Flask, render_template
 from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+
 app = Flask(__name__)
-app.url_map.strict_slashes = False
-app.jinja_env.trim_blocks = True
-app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def teardown_app(exception):
-    '''Calls Storage close on appcontext'''
+def winddown(exception):
+    """Closes the storage"""
     storage.close()
 
 
-@app.route('/cities_by_states')
-def cities_by_states():
-    states = []
-    for key, values in storage.all('State').items():
-        states.append(values)
-    cities = []
-    for key, values in storage.all('City').items():
-        cities.append(values)
-    return render_template('8-cities_by_states.html',
-                           states=states, cities=cities)
+@app.route("/cities_by_states", strict_slashes=False)
+def get_states():
+    """Returns an HTML page with a list of states"""
+
+    def sort_by_name(item):
+        """Sorting function"""
+        return item.name.upper()
+
+    stateslist = storage.all(State)
+    statesimple = [data for name, data in stateslist.items()]
+    statesimple.sort(key=sort_by_name)
+
+    return (render_template("8-cities_by_states.html", states=statesimple))
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port="5000")
